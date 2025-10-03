935bbd9ca947d2dcdd67d7777e8e38ca2489ec0c
Hong0206 <hongyd20042@gmail.com>
2025-10-03 21:12:11 +0900

    BOJ(5337) 웰컴
    문자열 출력을 할 줄 아느냐를 묻는 문제, 이스케이프 시퀀스를 사용해 특수문자를 적절히 출력할 수 있는지를 확인

---
+++ Implementation/B5_5337.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    // 속도 증가
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // 이스케이프 시퀀스 사용할 줄 아냐는 문제
+    cout << ".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.\n";
+
+    return 0;
+}
