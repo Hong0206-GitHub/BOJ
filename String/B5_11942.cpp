18405576176a740068268989990600812b74ac18
Hong0206 <hongyd20042@gmail.com>
2025-10-04 12:57:08 +0900

    BOJ(11942) 고려대는 사랑입니다
    고려대 사랑해요~!

---
+++ String/B5_11942.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    // 속도 증가
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // 답안 출력
+    cout << "고려대학교\n";
+
+    return 0;
+}
