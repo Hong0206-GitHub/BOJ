f20514624e7f1079a184af908242143662cb25f4
Hong0206 <hongyd20042@gmail.com>
2025-10-06 14:00:31 +0900

    BOJ(5338) 마이크로소프트 로고
    단순한 아스키 아트 그리기

---
+++ ASCII_Art/B5_5338.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    // 속도 증가
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+    
+    return 0;
+}
