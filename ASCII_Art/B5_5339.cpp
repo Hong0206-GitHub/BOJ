257633d9303e4ed43b7236e46aeb3ac533cfb06f
Hong0206 <hongyd20042@gmail.com>
2025-10-06 14:03:07 +0900

    BOJ(5339) 콜센터
    단순한 아스키 아트 그리기

---
+++ ASCII_Art/B5_5339.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+    // 속도 증가
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "     /~\\" << endl;
+    cout << "    ( oo|" << endl;
+    cout << "    _\\=/_" << endl;
+    cout << "   /  _  \\" << endl;
+    cout << "  //|/.\\|\\\\" << endl;
+    cout << " ||  \\ /  ||" << endl;
+    cout << "============" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+    
+    return 0;
+}
