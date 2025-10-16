eccca8ad951cab9fa44e79adc8a9bfe9df9cc640
Hong0206 <hongyd20042@gmail.com>
2025-10-16 22:15:08 +0900

    BOJ(3986) 좋은 단어
    입력 문자열의 길이가 짝수가 아니면 조건에 해당되지 않음
    
    스택이 비어있거나 스택의 최상단과 문자가 다른 경우 해당 문자 스택에 푸시
    스택이 비어있지 않으면서 스택의 최상단과 문자가 같은 경우 스택에서 팝
    이외의 케이스는 좋은 단어 조건에 해당하지 않는 경우임.

--- Stack/S4_3986.cpp
+++ Stack/S4_3986.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <stack>
 
 using namespace std;
@@ -9,49 +10,54 @@ int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    // 반복 횟수 입력 받기
+    // 테스트 케이스 입력
     int N;
     cin >> N;
 
     // 답안 저장용 변수
     int count = 0;
 
-    // N번 실행
-    while (--N >= 0) {
-        
-        // 단어 입력 받기
-        string word;
-        cin >> word;
+    // 테스트 케이스만큼 반복
+    while (N--) {
+        // 변수 초기화 및 새로운 값 저장
+        string str;
+        cin >> str;
 
-        // 조건 검사용 스택 선언
-        stack<char> stk;
-        
-        // 조건 검사용 불타입 변수 선언
+        // 분기 확인용 변수
         bool check = true;
 
-        // 단어의 한 글자씩
-        for (int i = 0; i < word.size(); ++i) {
+        // 스택 초기화
+        stack<char> s;
 
-            // 스택이 비어있거나, 스택의 top부분과 글자의 내용이 다르다면,
-            // 스택에 해당 글자 push
-            if (stk.empty() || stk.top() != word[i]) stk.push(word[i]);
-            // 스택의 최상단과 해당 글자가 같은 경우 스택에서 빼기
-            else if (stk.top() == word[i]) stk.pop();
-            // 두 경우에 해당되지 않으면, 반복문 종료 후 조건에서 제외
+        // 짝수가 아니면 애당초 성립이 안됨
+        if (str.size() % 2 != 0) {
+            continue;
+        }
+
+        // 모든 글자에 대해 반복
+        for (size_t i = 0; i < str.size(); i++) {
+            // 스택이 비어있거나, 스택의 탑과 값이 다르면 푸시
+            if (s.empty() || s.top() != str[i]) {
+                s.push(str[i]);
+            }
+        
+            // 스택이 비어있지 않으면서 스택의 탑과 값이 같은 경우
+            // 스택에서 팝
+            else if (str[i] == s.top()) s.pop();
+
+            // 이외의 케이스는
+            // 조건에 해당하지 않는 경우이므로 분기 조절 후 브레이크
             else {
                 check = false;
                 break;
             }
         }
-
-        // 스택에 내용물이 남아있는 경우에도 조건에서 제외
-        if (!stk.empty()) check = false;
-        // 모든 조건 통과시 카운터 증가
-        if (check) count++;
+        // 조건에 따라 카운트 증가
+        if (check && s.empty()) count++;
     }
 
     // 답안 출력
     cout << count << '\n';
-
+    
     return 0;
 }
