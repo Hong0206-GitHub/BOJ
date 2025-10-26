239f4d5c8d03fe6fea607c9e8647d0bc86d345d1
Hong0206 <hongyd20042@gmail.com>
2025-10-26 14:29:10 +0900

    BOJ(1850) 최대공약수
    유클리드 호제법 알고리즘을 사용하여, 최대공약수를 구하고, 그 수만큼의 1을 출력하는 문제

---
+++ Number_Theory/Euclidean_Algorithm/S1_1850.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+// 유클리드 호제법 알고리즘 사용
+int gcd(ll a, ll b) {
+    while (a % b != 0) {
+        ll temp = a % b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
+}
+
+int main(void) {
+
+    // 입출력 고속화
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    // 두 수 입력 받기
+    ll a, b;
+    cin >> a >> b;
+
+    // 최대공약수 구하기
+    ll g = gcd(a, b);
+
+    // 최대공약수 만큼 1 출력
+    for (ll i = 0; i < g; i++) cout << 1;
+    cout << '\n';
+
+    return 0;
+}
