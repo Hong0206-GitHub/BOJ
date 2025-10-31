8aab430a8506c9ae5eaf6492670a1de2758e1a93
Hong0206 <hongyd20042@gmail.com>
2025-11-01 01:09:53 +0900

    BOJ(32384) 사랑은 고려대입니다
    입력 받은 횟수만큼 문자열 출력

---
+++ Implementation/Bronze/B5_32384.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+
+    // 입출력 고속화
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    // 반복 횟수 입력 받기
+    int N;
+    cin >> N;
+
+    // 횟수만큼 출력 반복
+    for (int i = 0; i < N; i++) {
+        cout << "LoveisKoreaUniversity ";
+    }
+    cout << '\n';
+
+    return 0;
+}
