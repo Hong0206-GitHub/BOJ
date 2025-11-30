2fc6f786bd91f05a4c6e064e474cafaff84f9f12
Hong0206 <hongyd20042@gmail.com>
2025-11-30 16:44:31 +0900

    BOJ(9095) 1, 2, 3 더하기
    DP 기본 문제 바텀-업 방식과 탑-다운 방식 두 함수를 모두 구현

--- Dynamic_Programming/S3_9095.cpp
+++ Dynamic_Programming/S3_9095.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-// DP 함수
-int DP(int n, vector<int> &memo) {
+// DP(top_down 방식)
+int top_down(int n, vector<int> &memo) {
  
     // 기저조건
     if (n < 0) return 0;
@@ -13,9 +13,31 @@ int DP(int n, vector<int> &memo) {
     if (memo[n] != -1) return memo[n];
     
     // 점화식
-    memo[n] = DP(n - 1, memo) + DP(n - 2, memo) + DP(n - 3, memo);
+    memo[n] = top_down(n - 1, memo) + top_down(n - 2, memo) + top_down(n - 3, memo);
 
     return memo[n];
+}
+
+// DP(bottom_up 방식)
+int bottom_up(int n) {
+    // 기저 조건
+    if (n == 0) return 1;
+    else if (n < 0) return 0;
+
+    // 공간복잡도를 줄이기 위해 3개의 변수만 사용
+    // 문제 특성 상 하위 3개의 문제의 답안 사용하고 나머지는 버림
+    int temp1 = 0, temp2 = 0, temp3 = 1;
+    int current = 0;
+
+    // 반복문 사용(값 갱신)
+    for (int i = 1; i <= n; i++) {
+        current = temp1 + temp2 + temp3;
+        temp1 = temp2;
+        temp2 = temp3;
+        temp3 = current;
+    }
+
+    return current;
 }
 
 int main(void) {
@@ -36,7 +58,8 @@ int main(void) {
         vector<int> memo(N + 1, -1);
 
         // 재귀(탑 다운 방식)로 답안 출력
-        cout << DP(N, memo) << '\n';
+        cout << top_down(N, memo) << '\n';
+        //cout << botton_up(N) << '\n';   
     }
 
     return 0;
